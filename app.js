833c5c0f8f792b0b4ea6a6113436ff8c9479f4a2
Paul Hummer <paul@eventuallyanyway.com>
2011-12-10 12:28:21 -0300

    Create a combo loader node.js app.
    yaypie's comboloader takes care of all the heavy lifting here.  I just point to
    the current symlink and let it do its thing.

---
+++ app.js
@@ -0,0 +1,21 @@
+var combo = require('combohandler'),
+    express = require('express'),
+    app = express.createServer();
+
+app.configure(function() {
+    app.use(express.errorHandler());
+});
+
+app.error(function (err, req, res, next) {
+    if (err instanceof combo.BadRequest) {
+        res.send('Bad request.', {'Content-Type': 'text/plain'}, 400);
+    } else {
+        next();
+    }
+});
+
+app.get('/', combo.combine({rootPath: 'build/www'}), function (req, res) {
+    res.send(res.body, 200);
+});
+
+app.listen(8082);
