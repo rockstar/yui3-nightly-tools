5ba5dae082a9ac7911e3b998fd714e9f82662472
Paul Hummer <paul@eventuallyanyway.com>
2011-12-12 18:22:01 -0700

    Add a documentation page.
    It makes no sense to deploy this without documentation, so I made the / be a
    simple "this is how you do it".
    
    This meant that I needed to change the endpoint for the combo loader to work
    at.  It's now at /yui

--- app.js
+++ app.js
@@ -2,10 +2,16 @@
 
 var combo = require('combohandler'),
     express = require('express'),
+    stache = require('stache'),
     app = express.createServer();
 
 app.configure(function() {
-    app.use(express.errorHandler());
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'mustache');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.register('.mustache', stache);
+    app.use(express.static(__dirname + '/static'));
 });
 
 app.error(function (err, req, res, next) {
@@ -16,7 +22,10 @@ app.error(function (err, req, res, next) {
     }
 });
 
-app.get('/', combo.combine({rootPath: 'build/www'}), function (req, res) {
+app.get('/', function(req, res) {
+    res.render('index', {});
+});
+app.get('/yui', combo.combine({rootPath: 'build/www'}), function (req, res) {
     res.send(res.body, 200);
 });
 
